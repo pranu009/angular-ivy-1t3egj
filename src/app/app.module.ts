fb067cf06a4b8c727359cbede0386be47dd8529f
pranu009 <pranalipawar91194@gmail.com>
2022-02-20 20:55:14 +0530

    6 files modified, 2 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,25 +7,25 @@ import { HomeComponent } from './home/home.component';
 import { SearchComponent } from './search/search.component';
 import { HistoryComponent } from './history/history.component';
 import { FormsModule } from '@angular/forms';
-import { CommonModule } from "@angular/common";
+import { CommonModule } from '@angular/common';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
+import { HomeModule } from './home/home.module';
+import { ShellModule } from './shell/shell.module';
+
 @NgModule({
-  declarations: [
-    AppComponent,
-    HomeComponent,
-    SearchComponent,
-    HistoryComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
     CommonModule,
     FormsModule,
-    NgbModule
+    NgbModule,
+    HomeModule,
+    ShellModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
